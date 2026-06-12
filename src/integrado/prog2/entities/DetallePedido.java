72197d0574364e2332b271086f0fcd7c7ba102f9
Leonel <leonelponce0001@gmail.com>
2026-06-12 19:44:31 -0300

    Crear clase DetallePedido

---
+++ src/integrado/prog2/entities/DetallePedido.java
@@ -0,0 +1,38 @@
+package integrado.prog2.entities;
+
+public class DetallePedido extends Base{
+    private int cantidad;
+    private Double subtotal;
+    private Producto producto;
+
+    //Constructor vacío
+    public DetallePedido(){
+        super();
+    }
+    //Constructor con todos los parametros
+    public DetallePedido(int cantidad, Double subtotal, Producto producto){
+        super();
+        this.cantidad = cantidad;
+        this.subtotal = subtotal;
+        this.producto = producto;
+    }
+
+    //Getters & Setters
+
+    public int getCantidad() {return cantidad;}
+    public void setCantidad(int cantidad) {this.cantidad = cantidad;}
+
+    public Double getSubtotal() {return subtotal;}
+    public void setSubtotal(Double subtotal) {this.subtotal = subtotal;}
+
+    public Producto getProducto() {return producto;}
+    public void setProducto(Producto producto) {this.producto = producto;}
+
+    @Override
+    public String toString() {
+        String prodNombre = (producto != null) ? producto.getNombre() : "Sin producto";
+        return "DetallePedido [Cantidad=" + cantidad + ", Subtotal=$" + subtotal + ", Producto=" + prodNombre + "]";
+    }
+}
+
+
